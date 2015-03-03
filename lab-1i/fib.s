b275b4b10de134593b7bfc0ee7c6d11247ae0b3a
harvey <harvey@hotmail.com>
2015-03-03 23:24:48 +0800

    revise

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -9,35 +9,42 @@
 	.type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
+	push { lr}
+        subs r4, r0, #0        // r4 = r0 - 0
+        ble .L3        //if (r0 <= 0) goto .L3
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+        cmp r4, #1        // Compare r4 to 1
+        beq .L4        // if (r4 == 1) goto .L4
+	//moveq r0, #1
+ 	sub r4,r4,#1 //r4=r4-1 
+	mov  r6,#1
+        mov  r7,#0     
+l1:     
+	cmp r6,r7
+	ite gt
+	addgt r7,r6,r7
+	addle r6,r6,r7
+	subs r4,r4,#1 //r4=r4-1 
+	bne  l1
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+	cmp r6,r7
+	ite gt
+	movgt r0,r6
+	movle r0,r7
+	
+	pop {  pc}
 
 	@ END CODE MODIFICATION
 .L3:
 	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
+pop {  pc}
+	
 
 .L4:
 	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+pop {  pc}
+	
 
 	.size fibonacci, .-fibonacci
 	.end
--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -7,8 +7,12 @@ int main(int argc, char **argv)
   int number=0;
   int result=0;
 
-  scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
+  if (scanf("%d", &number) == 1) {
+    	result = fibonacci(number);   
+  	printf("The fibonacci sequence at %d is: %d\n", number,result);
+    } else {
+        printf("failed to read integer.\n");
+    }
+	return 1;
 }
 
