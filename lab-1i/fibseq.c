18fddf382874c8d244277b36d6da84cbd4c7a0d1
harvey <harvey@hotmail.com>
2015-03-04 12:18:02 +0800

    delete the enumeration

--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
     	result = fibonacci(number);   
   	printf("The fibonacci sequence at %d is: %d\n", number,result);
     } else {
-        printf("failed to read integer.\n");
+        return 2;
     }
 	return 1;
 }
