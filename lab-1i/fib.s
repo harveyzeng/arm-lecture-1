09a4a7c275fe84ca747337da6bd25098d51ac7ed
harvey <harvey@hotmail.com>
2015-03-04 12:10:29 +0800

    add the Comment

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -9,29 +9,29 @@
 	.type fibonacci, function
 
 fibonacci:
-	push { lr}
+	push { lr}            //push the link register to the stack
         subs r4, r0, #0        // r4 = r0 - 0
         ble .L3        //if (r0 <= 0) goto .L3
 
         cmp r4, #1        // Compare r4 to 1
         beq .L4        // if (r4 == 1) goto .L4
-	//moveq r0, #1
+	
  	sub r4,r4,#1 //r4=r4-1 
-	mov  r6,#1
-        mov  r7,#0     
+	mov  r6,#1   //r6=1 
+        mov  r7,#0    //r7=0 
 l1:     
-	cmp r6,r7
-	ite gt
-	addgt r7,r6,r7
-	addle r6,r6,r7
-	subs r4,r4,#1 //r4=r4-1 
-	bne  l1
+	cmp r6,r7    //compare r6 r7
+	ite gt        //if then condition
+	addgt r7,r6,r7 //if(r6>r7) r7=r6+r7
+	addle r6,r6,r7 //if(r7>r6) r6=r6+r7
+	subs r4,r4,#1 //r4=r4-1 renew the flag 
+	bne  l1        //if(r4!=0) go to l1
 
 
-	cmp r6,r7
-	ite gt
-	movgt r0,r6
-	movle r0,r7
+	cmp r6,r7 //compare r6 r7
+	ite gt    //if then condition
+	movgt r0,r6 //if(r6>r7) r0=r6
+	movle r0,r7 //if(r7>r6) r0=r7
 	
 	pop {  pc}
 
